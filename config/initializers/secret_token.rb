ec3f9a067cc79a27d2caf83e41eead654e77dc33
Brandon Hilkert <brandonhilkert@gmail.com>
2013-06-17 16:33:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Illuminati::Application.config.secret_key_base = '835d641dcbe664fa31731f5076b76353aef6224331392b7672c75dd7e57dedac6b44e922a189778efeee8df676b6a37657204d0c3849cacc38dd8fe3a34c5eb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Illuminati::Application.config.session_store :cookie_store, key: '_illuminati_session'
