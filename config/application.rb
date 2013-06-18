f81d1f710275c32be1c52be79320da03fbdb7664
Brandon Hilkert <brandonhilkert@gmail.com>
2013-06-18 13:58:31 -0400

    Don't break when application.yml isn't there

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,12 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value unless value.kind_of? Hash
+if File.exists?(File.expand_path('../application.yml', __FILE__))
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value unless value.kind_of? Hash
+  end
 end
 
 # Require the gems listed in Gemfile, including any gems
