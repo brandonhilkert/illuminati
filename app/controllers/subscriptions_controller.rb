9ff239aceffdbc6bb6e29573609df5de8113fa05
Brandon Hilkert <brandonhilkert@gmail.com>
2013-06-18 13:48:31 -0400

    Update

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class SubscriptionsController < ApplicationController
+  before_action :find_subscription, only: [:edit, :update, :destroy]
+
   def new
     @subscription = Subscription.new
   end
@@ -14,14 +16,11 @@ class SubscriptionsController < ApplicationController
   end
 
   def edit
-    @subscription = Subscription.find_by_id(params[:id])
     redirect_to new_subscription_path, notice: "We were unable to find your subscription." unless @subscription.present?
   end
 
   def update
-    subscription = Subscription.find_by_id(params[:id])
-
-    if subscription.update(subscription_params)
+    if @subscription.update(subscription_params)
       redirect_to done_subscriptions_path
     else
       render :edit
@@ -29,10 +28,19 @@ class SubscriptionsController < ApplicationController
 
   end
 
+  def destroy
+    @subscription.destroy
+    redirect_to new_subscription_path, notice: "You're successfully unsubscribed from your newsletter."
+  end
+
   def done
   end
 
   private
+
+  def find_subscription
+    @subscription = Subscription.find_by_id(params[:id])
+  end
 
   def subscription_params
     params.require(:subscription).permit(:email, :frequency_id, :topic_1, :topic_2, :topic_3)
